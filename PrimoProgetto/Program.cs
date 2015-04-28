640b1d56603297d865b63817b3578be5507abf29
AndreaPretini <pretini.andrea@gmail.com>
2015-04-28 15:25:17 +0200

    Primo Commit

---
+++ PrimoProgetto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimoProgetto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciao a tutti");
+            Console.ReadLine();
+        }
+    }
+}
